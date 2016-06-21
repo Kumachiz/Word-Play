ba9421a2710971ca154f6e2d41b6f7c26bce2596
April Peng and Michael Munyaneza <helloaprilpeng@gmail.com>
2016-06-20 19:37:19 -0700

    complete index.html content. complete scripts.js logic to include all leap year specifications

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
-      if ((year % 100 !== 0) && (year % 4 === 0)) {
+      if ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
         return true;
       } else {
         return false;
@@ -11,7 +11,15 @@ $(function() {
 
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
 
   });
 });
